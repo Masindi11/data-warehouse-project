c90f2f65cf01161f4198151d9dec6593ec421eef
Masindi11 <gaylemasindi@gmail.com>
2026-08-30 16:06:39 +0200

    Add dwh_create_date column to multiple tables

--- Scripts/silver/ddl_silver.sql
+++ Scripts/silver/ddl_silver.sql
@@ -12,6 +12,8 @@ Run this script to re-define the DDL structure of 'bronze' Tables
 
 IF OBJECT_ID ('silver.crm_cust_info' , 'U') IS NOT NULL
    DROP TABLE silver.crm_cust_info;
+GO
+
 CREATE TABLE silver.crm_cust_info (
     cst_id INT,
     cst_key            NVARCHAR(50),
@@ -19,9 +21,11 @@ CREATE TABLE silver.crm_cust_info (
     cst_lastname       NVARCHAR(50),
     cst_material_status NVARCHAR(50),
     cst_gndr           NVARCHAR(50),
-    cst_create_date    DATE
+    cst_create_date    DATE,
+dwh_create_date       DATETIME2 DEFAULT GETDATE()
     
 );
+GO
 
 IF OBJECT_ID ('silver.crm_prd_info' , 'U') IS NOT NULL
    DROP TABLE silver.crm_prd_info;
@@ -33,7 +37,9 @@ CREATE TABLE silver.crm_prd_info (
     prd_line       NVARCHAR(50),
     prd_start_dt   DATETIME,
     prd_end_dt     DATETIME,
+   dwh_create_date       DATETIME2 DEFAULT GETDATE()
 );
+GO
 
 IF OBJECT_ID ('silver.crm_sales_details' , 'U') IS NOT NULL
    DROP TABLE silver.crm_sales_details;
@@ -47,17 +53,19 @@ CREATE TABLE silver.crm_sales_details (
         sls_sales     INT,
         sls_quantity  INT,
         sls_price     INT,
-        
+       dwh_create_date       DATETIME2 DEFAULT GETDATE() 
 
 );
+GO
 
 IF OBJECT_ID ('silver.erp_loc_a101' , 'U') IS NOT NULL
    DROP TABLE silver.erp_loc_a101;
 CREATE TABLE silver.erp_loc_a101 (
       cid   NVARCHAR(50),
       cntry NVARCHAR(50),
-
+      dwh_create_date       DATETIME2 DEFAULT GETDATE()
 );
+GO
 
 IF OBJECT_ID ('silver.erp_cust_az12' , 'U') IS NOT NULL
    DROP TABLE silver.erp_cust_az12;
@@ -65,8 +73,9 @@ CREATE TABLE silver.erp_cust_az12 (
       cid    NVARCHAR(50),
       bdate  DATE,
       gen    NVARCHAR(50)
-
+      dwh_create_date       DATETIME2 DEFAULT GETDATE()
 );
+GO
 
 IF OBJECT_ID ('silver.erp_Px_cat_g1v2' , 'U') IS NOT NULL
    DROP TABLE silver.erp_Px_cat_g1v2;
@@ -75,5 +84,6 @@ CREATE TABLE silver.erp_Px_cat_g1v2 (
      cat      NVARCHAR(50),
      subcat   NVARCHAR(50),
      maintenance NVARCHAR(50),
-
+     dwh_create_date       DATETIME2 DEFAULT GETDATE()
 );
+GP
